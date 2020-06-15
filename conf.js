eab3dd2c391bbd87fe03bd573d23f0b09e73b75e
Yamcha973 <61129914+Yamcha973@users.noreply.github.com>
2020-06-15 21:14:15 +0200

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,8 +1,8 @@
 const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
-user :  'root', // le nom d'utilisateur
-password :  'yamcha_server97', // le mot de passe
+user :  '*****', // le nom d'utilisateur
+password :  '******', // le mot de passe
 database :  'fil_rouge', // le nom de la base de données
 });
 module.exports = connection;
